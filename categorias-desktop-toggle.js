d169b7b1e35264d9728cbd25f9911c9f73cf830f
Luis Fernando <serraluisfernando@gmail.com>
2020-03-28 01:50:21 -0300

    ajuste alt pagina produto

--- categorias-desktop-toggle.js
+++ categorias-desktop-toggle.js
@@ -12,12 +12,13 @@ $(document).ready(function(){
         
         $(window).scroll(function(){
           var topo = $(window).scrollTop();
-          if(topo<155){   
+          if(topo<151){   
             $('.barshomedesk').css({"display":"none"});
             $('.header-inner #header-right-cart').css({"margin-top":"20px"});
             $('.header-inner .icon-shopping-cart').css({"font-size":"73px"});
             $('.header-inner .header-cart ').css({"top":"0"});
             $('.desktopclose').css({"display":"none"});
+            $('#product .offcanvas-siderbars').css({"padding-top":"0"});
             $('.header-inner .logo-theme img').css({"width":"170px"});
             $('.barshomedesk').addClass("icon-bars");
             $('#wbc-mainnav').removeClass("activetog");
@@ -27,6 +28,7 @@ $(document).ready(function(){
                     
             $('.barshomedesk').css({"display":"block"});
             $('.header-inner .logo-theme img').css({"width":"123px"});
+            $('#product .offcanvas-siderbars').css({"padding-top":"250px"});
             $('.header-inner #header-right-cart').css({"margin-top":"30px"});
             $('.header-inner .icon-shopping-cart').css({"font-size":"60px"});
             $('.header-inner .header-cart').css({"top":"10px"});
@@ -67,8 +69,9 @@ $(document).ready(function(){
         
         	$(window).scroll(function(){
           	var topo = $(window).scrollTop();
-          		if(topo<155){
+          		if(topo<151){
                 	$('.barshomedesk').css({"display":"none"});
+                   $('#product .offcanvas-siderbars').css({"padding-top":"0"});
                   $('.header-inner .logo-theme img').css({"width":"170px"});
                 	$('.header-inner #header-right-cart').css({"margin-top":"20px"});
                 	$('.header-inner .icon-shopping-cart').css({"font-size":"73px"});
@@ -80,6 +83,7 @@ $(document).ready(function(){
 
           		else{      
             		$('.barshomedesk').css({"display":"block"});
+                $('#product .offcanvas-siderbars').css({"padding-top":"250px"});
                 $('.header-inner .logo-theme img').css({"width":"123px"});
             		$('.header-inner #header-right-cart').css({"margin-top":"30px"});
             		$('.header-inner .icon-shopping-cart').css({"font-size":"60px"});
