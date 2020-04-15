dbc718ef426553cac226bc45a468cf98fd6bdc29
Luis Fernando <serraluisfernando@gmail.com>
2020-04-15 17:42:31 -0300

    ajuste em barra movel e desk

--- barra-buscas-mobile.js
+++ barra-buscas-mobile.js
@@ -2,16 +2,8 @@ $(document).ready(function(){
 
 		//Barra de Categorias e Buscas
 		//Barra de Categorias
-		if (window.matchMedia('(max-width: 1024px)').matches){  
-  			
-  			$('.wbc-mainnav').css({"margin-top":"0"});  
-            $('#header-search').css({"margin-top":"0"});	
-        //Botão Fechar Toggle
-  			if ($(window).width() <= 1023){  
-				
-				
-				
-        		
+		if (window.matchMedia('(max-width: 1023px)').matches){  
+  			if ($(window).width() <= 1023){          		
 				$('.menu-mobile').append('<i class="fas fa-times"></i>');
 				$('.fa-times').addClass('closemobile');
 				$('.closemobile').css({"display":"none"});
@@ -35,15 +27,7 @@ $(document).ready(function(){
 
 				});
 			
-			}else{
-
-        $('.closemobile').css({"display":"none"});
-        $('.fa-times').removeClass('closemobile');
-      }
-		
-      			
-
-  			
+			}  			
 		}
 
 		//Barra de Buscas
@@ -51,30 +35,21 @@ $(document).ready(function(){
 			//Removedor de Classes
 			$('#header-search').removeClass("col-md-offset-1 col-md-6 col-sm-12 col-xs-12");	
 			$('#suggestions1').removeClass("col-lg-10 col-md-10 col-sm-12 col-xs-12");
-			$('#wbc-mainnav').addClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
+			$('#wbc-mainnav').addClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");			
+		}
 
-			
-					}
-		else{
-	
+		else{	
 			//Adicionador de Classes
 			$('#header-search').addClass("col-md-offset-1 col-md-6 col-sm-12 col-xs-12");
 			$('#suggestions1').addClass("col-lg-10 col-md-10 col-sm-12 col-xs-12");
 			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
 		}
 
-		//Ajuste da Barra de Buscas na Resolução de 999px
-		if (window.matchMedia('(max-width: 999px)').matches){  
-
 		
-		}
 		//Ajuste da Barra de Buscas na Resolução de 992px
 		if (window.matchMedia('(max-width: 992px)').matches){  
 
-			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
-			
-			
-			
+			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");			
 		}
 
 		//Adicionador de Classe a Barra de Busca em Resoluçoes Acima de 992px
@@ -86,33 +61,14 @@ $(document).ready(function(){
 		//Ajuste da Barra de Buscas na Resolução de 991px
 		if (window.matchMedia('(max-width: 991px)').matches){  
 
-			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
-
-			
-	
-			
+			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");			
 		}
 
 				
 		// Funçao Resize da Barra de Buscas e Categorias
 		$(window).resize(function(){
-			if (window.matchMedia('(max-width: 1024px)').matches){  
-  			
-  			
-      
+			
 		
-      		
-		}	
-
-
-			//Barra de Categorias e Buscas
-		//Barra de Categorias
-		if (window.matchMedia('(max-width: 1023px)').matches){  
-  			
-		
-
-		}
-
 		//Barra de Buscas
 		if (window.matchMedia('(max-width: 1023px)').matches){  
 			//Removedor de Classes
@@ -132,17 +88,11 @@ $(document).ready(function(){
 			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
 		}
 
-		//Ajuste da Barra de Buscas na Resolução de 999px
-		if (window.matchMedia('(max-width: 999px)').matches){  
-			
-		}
+		
 		//Ajuste da Barra de Buscas na Resolução de 992px
 		if (window.matchMedia('(max-width: 992px)').matches){  
 
-			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
-
-			
-			
+			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");			
 		}
 
 		//Adicionador de Classe a Barra de Busca em Resoluçoes Acima de 992px
@@ -154,10 +104,7 @@ $(document).ready(function(){
 		//Ajuste da Barra de Buscas na Resolução de 991px
 		if (window.matchMedia('(max-width: 991px)').matches){  
 
-			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");
-
-			
-			
+			$('#wbc-mainnav').removeClass(" col-md-12 col-sm-12 hidden-sm hidden-xs");			
 		}
-		});
+	});
 });
