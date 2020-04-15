f4673c704646943aed663f524625fcfbba3be840
Luis Fernando <serraluisfernando@gmail.com>
2020-04-15 17:25:58 -0300

    tirar funcao resize

--- categorias-desktop-toggle.js
+++ categorias-desktop-toggle.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-    $('.desktopclose').css({"display":"none"});
-    $('.barshomedesk').css({"display":"none"});
-
     if (window.matchMedia('(max-width: 1024px)').matches){
       if ($(window).width() > 1023){  
 
@@ -91,51 +88,5 @@ $(document).ready(function(){
     }
 
     
-    $(window).resize(function(){        
-        $(window).scroll(function(){
-          var topo = $(window).scrollTop();
-          if(topo<151){   
-            $('.barshomedesk').css({"display":"none"});
-            $('.header-inner #header-right-cart').css({"margin-top":"20px"});
-            $('.header-inner .icon-shopping-cart').css({"font-size":"73px"});
-            $('.header-inner .header-cart ').css({"top":"0"});
-            $('.desktopclose').css({"display":"none"});
-            $('#product .offcanvas-siderbars').css({"padding-top":"0"});
-            $('#category .offcanvas-siderbars').css({"padding-top":"0"});
-            $('.header-inner .logo-theme img').css({"width":"170px"});
-            $('.barshomedesk').addClass("icon-bars");
-            $('#wbc-mainnav').removeClass("activetog");
-            $('.wbc-mainnav').css({"margin-top":"0"});
-          }
-
-          else{
-                    
-            $('.barshomedesk').css({"display":"block"});
-            $('.header-inner .logo-theme img').css({"width":"123px"});
-            $('#product .offcanvas-siderbars').css({"padding-top":"250px"});
-            $('#category .offcanvas-siderbars').css({"padding-top":"250px"});
-            $('.header-inner #header-right-cart').css({"margin-top":"30px"});
-            $('.header-inner .icon-shopping-cart').css({"font-size":"60px"});
-            $('.header-inner .header-cart').css({"top":"10px"});
-            ;
-          }
-      });
-
-      $('.barshomedesk').click(function(){
-          $('#wbc-mainnav').toggleClass('activetog');
-          $('.desktopclose').css({"display":"block"});
-          $('.barshomedesk').css({"display":"none"});
-          $('.barshomedesk').removeClass("icon-bars");
-          $('.wbc-mainnav').css({"margin-top":"140px"});
-      });
-
-      $('.desktopclose').click(function(){
-          $('#wbc-mainnav').toggleClass('activetog');
-          $('.desktopclose').css({"display":"none"});
-          $('.barshomedesk').css({"display":"block"});
-          $('.barshomedesk').addClass("icon-bars");
-          $('.wbc-mainnav').css({"margin-top":"0"});
-      });     
-     
-	});    
+    
 });
