7d310d47da2a13603241f7c87e2f1179ef998d00
Luis Fernando <serraluisfernando@gmail.com>
2020-04-20 11:30:44 -0300

    ajuste com resize

--- categorias-desktop-toggle.js
+++ categorias-desktop-toggle.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
      if ($(window).width() > 1023){        
       $('.header-inner').append('<i class="icon-bars barshomedesk"></i>');
-      $('.header-inner').append('<i class="fas fa-times desktopclose"></i>')
+      $('.header-inner').append('<i class="fas fa-times desktopclose"></i>');
 
      $('.barshomedesk').css({"display":"none"});
      $('.icon-bars').css({"display":"none"});
@@ -89,10 +89,42 @@ $(document).ready(function(){
         $('.icon-shopping-cart').addClass("icon-desk-cart");
         $('.header-cart').addClass("header-desk-cart");
      }
-     
+
+       if ($(window).width() > 1023){        
+      
     
 
 
+        $(window).scroll(function(){
+          var topo = $(window).scrollTop();
+          if(topo<151){   
+            $('.header-desk-cart').css({"top":"0"});
+            $('.barshomedesk').css({"display":"none"});
+            $('.header-inner #header-right-cart').css({"margin-top":"20px"});
+            $('.header-inner .icon-desk-cart').css({"font-size":"73px"});
+            $('.header-inner .header-cart ').css({"top":"0"});
+            $('.desktopclose').css({"display":"none"});
+            $('#product .offcanvas-siderbars').css({"padding-top":"0"});
+            $('#category .offcanvas-siderbars').css({"padding-top":"0"});
+            $('.header-inner .logo-theme img').css({"width":"170px"});
+            $('.barshomedesk').addClass("icon-bars");
+            $('#wbc-mainnav').removeClass("activetog");
+            $('.wbc-mainnav').css({"margin-top":"0"});
+          }
 
+          else{
+                    
+            $('.barshomedesk').css({"display":"block"});
+            $('.header-desk-cart').css({"top":"10px"});
+            $('.header-inner .logo-theme img').css({"width":"123px"});
+            $('#product .offcanvas-siderbars').css({"padding-top":"250px"});
+            $('#category .offcanvas-siderbars').css({"padding-top":"250px"});
+            $('.header-inner #header-right-cart').css({"margin-top":"30px"});
+            $('.header-inner .icon-desk-cart').css({"font-size":"60px"});
+            $('.header-inner .header-cart').css({"top":"10px"});
+            ;
+          }
+      });     
+    } 
   });
 });
