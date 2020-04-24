8faf30dc34d2fb432e445627139d083ad35429ae
Luis Fernando <serraluisfernando@gmail.com>
2020-04-24 12:04:39 -0300

    ajuste final vao logo

--- categorias-desktop-toggle.js
+++ categorias-desktop-toggle.js
@@ -147,7 +147,7 @@ $(document).ready(function(){
             $('.barshomedesk').addClass("icon-bars");
             $('#wbc-mainnav').removeClass("activetog");
             $('#wbc-mainnav').css({"margin-top":"0"});
-            $('#searchbox').css({"margin-top":"0"});
+            $('#searchbox').css({"margin-top":"50px"});
           }
 
           else{
@@ -160,7 +160,7 @@ $(document).ready(function(){
             $('.header-inner #header-right-cart').css({"margin-top":"30px"});
             $('.header-inner .icon-desk-cart').css({"font-size":"60px"});
             $('.header-inner .header-cart').css({"top":"10px"});
-            $('#searchbox').css({"margin-top":"0"});
+            $('#searchbox').css({"margin-top":"50px"});
             ;
           }
       });
