50e5f42ad4b35b9dfa287d56f3639aecfdc26d47
Luis Fernando <serraluisfernando@gmail.com>
2020-04-25 01:36:12 -0300

    funcao remoedora com resize ratings

--- removedor-ratings.js
+++ removedor-ratings.js
@@ -23,7 +23,24 @@ $(document).ready(function(){
 
 	// Funçao Resize da Barra de Buscas e Categorias
 	$(window).resize(function(){
+		if ($(window).width() <= 1023){          		
+  		$('.reviews-modal-av-title .form-group div').removeClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+  		$('.reviews-modal-av-op .form-group div').removeClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+  		$('.reviews-modal-av-apelido .form-group div').removeClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+  		$('.reviews-modal-av-time .form-group div').removeClass("col-lg-4 col-md-4 col-sm-6 col-xs-12");
 			
+			
+
+	} 
+
+
+	else{
+		$('.reviews-modal-av-title .form-group div').addClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+		$('.reviews-modal-av-op .form-group div').addClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+		$('.reviews-modal-av-apelido .form-group div').addClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+		$('.reviews-modal-av-time .form-group div').addClass("col-lg-4 col-md-4 col-sm-6 col-xs-12");
+
+	} 					
 
 	});
 });
