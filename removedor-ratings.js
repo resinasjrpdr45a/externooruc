1c908c208ce9a5055f5df0f9320114eb19821933
Luis Fernando <serraluisfernando@gmail.com>
2020-04-25 01:23:16 -0300

    ajuste ratings teste

---
+++ removedor-ratings.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	
+  	if ($(window).width() <= 1023){          		
+  		$('.reviews-modal-av-title .form-group .clearfix div').removeClass("col-lg-6 .col-md-6 col-sm-8 col-xs-12");
+			
+			
+
+	} 
+
+
+	else{
+		$('.reviews-modal-av-title .form-group .clearfix div').addClass("col-lg-6 .col-md-6 col-sm-8 col-xs-12");
+
+	} 			
+		
+
+	// Funçao Resize da Barra de Buscas e Categorias
+	$(window).resize(function(){
+			
+
+	});
+});
