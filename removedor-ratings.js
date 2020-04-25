1aebe3ff3bc29903b3358e32bb0ae637f715b71b
Luis Fernando <serraluisfernando@gmail.com>
2020-04-25 01:34:13 -0300

    ajuste ratings final

--- removedor-ratings.js
+++ removedor-ratings.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
 	
   	if ($(window).width() <= 1023){          		
   		$('.reviews-modal-av-title .form-group div').removeClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+  		$('.reviews-modal-av-op .form-group div').removeClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+  		$('.reviews-modal-av-apelido .form-group div').removeClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+  		$('.reviews-modal-av-time .form-group div').removeClass("col-lg-4 col-md-4 col-sm-6 col-xs-12");
 			
 			
 
@@ -11,6 +14,9 @@ $(document).ready(function(){
 
 	else{
 		$('.reviews-modal-av-title .form-group div').addClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+		$('.reviews-modal-av-op .form-group div').addClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+		$('.reviews-modal-av-apelido .form-group div').addClass("col-lg-6 col-md-6 col-sm-8 col-xs-12");
+		$('.reviews-modal-av-time .form-group div').addClass("col-lg-4 col-md-4 col-sm-6 col-xs-12");
 
 	} 			
 		
