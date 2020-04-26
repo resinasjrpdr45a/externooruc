b00217f03d184512bc739c77b89aaf6dda27e95e
Luis Fernando <serraluisfernando@gmail.com>
2020-04-26 02:04:31 -0300

    ajuste final newsletter

--- newsletter.js
+++ newsletter.js
@@ -1,6 +1,73 @@
 $(document).ready(function(){
 	$('.icon-newsletter img').attr('src', 'https://www.jrresinas.oruc.com.br/arquivos/bi/1582235045_emailicompng.png');
+
+	('.footer-top .newsletter .btn-primary span').removeClass("hidden-xs hidden-sm");
+$(' #form_whatsapp span').removeClass("hidden-xs hidden-sm");
+$(' #form_newsletter span').removeClass("hidden-xs hidden-sm");
+
+if ($(window).width() <= 992)
+{  
+
+$('.footer-middle .space-bottom-5 .col-lg-4,.col-md-4,.col-sm-4,.col-xs-12 hidden-xs').addClass("creditcopy");
+$('.creditcopy').removeClass("col-lg-4 col-md-4 col-sm-4 col-xs-12 hidden-xs "); 
+
+$('.footer-middle .col-lg-3:nth-child(4),.col-md-3:nth-child(4),.col-sm-12:nth-child(4),.col-xs-12:nth-child(4)').css({"display":"none"});
+
+   $('#form_whatsapp .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
+   $('#form_whatsapp .block_content:nth-child(3)').removeClass("col-xs-2 col-sm-2 col-md-2");
+   $('#form_newsletter .block_content:nth-child(1)').removeClass("col-xs-12 col-sm-12 col-md-5");
+   $('#form_newsletter .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
+   $('#form_newsletter .block_content:nth-child(3)').removeClass("col-xs-2 col-sm-2 col-md-2");
+   $('.text-center').removeClass("col-xs-4 col- col-sm-12 col-md-5");
+
+$('.col-newsletter').removeClass( "col-lg-6 col-md-6 col-sm-6 col-xs-12" );
+   $('.col-news').removeClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
+   $('#form_whatsapp .block_content:nth-child(1)').removeClass("col-xs-12sm-4 col-md-3 pull-left");  
+   $('.footer-middle .payment').removeClass("pull-left");
+
+} 
+
+$(window).resize(function(){
 	
+  if ($(window).width() <= 992)
+{  
+$('.footer-middle .col-lg-3:nth-child(4),.col-md-3:nth-child(4),.col-sm-12:nth-child(4),.col-xs-12:nth-child(4)').css({"display":"none"});    
+$('.col-newsletter').removeClass( "col-lg-6 col-md-6 col-sm-6 col-xs-12" );
+   $('.col-news').removeClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
+   $('#form_whatsapp .block_content:nth-child(1)').removeClass("col-xs-12 col-sm-12 col-md-5");
+   $('#form_whatsapp .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
+   $('#form_whatsapp .block_content:nth-child(3)').removeClass("col-xs-2 col-sm-2 col-md-2");
+   $('#form_newsletter .block_content:nth-child(1)').removeClass("col-xs-12 col-sm-12 col-md-5");
+   $('#form_newsletter .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
+   $('#form_newsletter .block_content:nth-child(3)').removeClass("col-xs-2 col-sm-2 col-md-2");
+
+
+
+
+
+} else{
+   $('.col-newsletter').addClass( "col-lg-6 col-md-6 col-sm-6 col-xs-12" );
+   $('.col-news').addClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
+   $('#form_whatsapp .block_content:nth-child(1)').addClass("col-xs-12 col-sm-12 col-md-5");
+   $('#form_whatsapp .block_content:nth-child(2)').addClass("col-xs-10 col-sm-10 col-md-5");
+   $('#form_whatsapp .block_content:nth-child(3)').addClass("col-xs-2 col-sm-2 col-md-2");
+   $('#form_newsletter .block_content:nth-child(1)').addClass("col-xs-12 col-sm-12 col-md-5");
+   $('#form_newsletter .block_content:nth-child(2)').addClass("col-xs-10 col-sm-10 col-md-5");
+   $('#form_newsletter .block_content:nth-child(3)').addClass("col-xs-2 col-sm-2 col-md-2");
+
+
+}
+	
+
+   
+
+
+});
+
+  
+
+
+
 
 
 
