a4ee1250e8d1378fbfaddf5efaa2a299d4b43cfb
Luis Fernando <serraluisfernando@gmail.com>
2020-04-27 02:22:51 -0300

    colocacao final

--- newsletter.js
+++ newsletter.js
@@ -9,6 +9,8 @@ $('.newsletter .text-center').removeClass("col-xs-4 col-sm-4 col-md-3 pull-left"
 $('.block_content ').removeClass("col-xs-8 col-sm-8 col-md-9 pull-left");
 $('.col-form-newsletter').removeClass("col-xs-12");
 $('#form_whatsapp .block_content:nth-child(1)').removeClass("col-xs-12 col-sm-12 col-md-5 ");
+$('#form_whatsapp .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
+$('#form_whatsapp .block_content:nth-child(3)').removeClass("col-xs-2 col-sm-2 col-md-2");
 
 
 
@@ -21,7 +23,9 @@ $('.col-newsletter').addClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
 $('.newsletter .text-center').addClass("col-xs-4 col-sm-4 col-md-3 pull-left");
 $('.block_content ').addClass("col-xs-8 col-sm-8 col-md-9 pull-left");
 $('.col-form-newsletter').addClass("col-xs-12");
-$('#form_whatsapp .block_content:nth-child(1)').addClass("col-xs-12 col-sm-12 col-md-5 ");
+$('#form_whatsapp .block_content:nth-child(1)').addClass("col-xs-12 col-sm-12 col-md-5");
+$('#form_whatsapp .block_content:nth-child(2)').addClass("col-xs-10 col-sm-10 col-md-5");
+$('#form_whatsapp .block_content:nth-child(3)').addClass("col-xs-2 col-sm-2 col-md-2");
 
 }
 
