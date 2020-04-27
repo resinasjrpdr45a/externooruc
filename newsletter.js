2b65b690754cae0e636a22fa00fd150f6f72aec2
Luis Fernando <serraluisfernando@gmail.com>
2020-04-27 08:57:41 -0300

    ajuste block 1

--- newsletter.js
+++ newsletter.js
@@ -3,6 +3,8 @@ $('.icon-newsletter img').attr('src', 'https://www.jrresinas.oruc.com.br/arquivo
 
 $('.newsletter .block_content ').addClass("block1");
 $('#form_whatsapp .block_content').removeClass("block1");
+$('#form_newsletter .block_content').removeClass("block1");
+
 if (window.matchMedia('(max-width: 991px)').matches){
  
 $('.col-newsletter').removeClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
