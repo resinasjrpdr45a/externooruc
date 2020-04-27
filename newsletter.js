48355bd4d5da1c4d916eadb9c9e27588907c9800
Luis Fernando <serraluisfernando@gmail.com>
2020-04-27 08:43:27 -0300

    ajuste block 1

--- newsletter.js
+++ newsletter.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 $('.icon-newsletter img').attr('src', 'https://www.jrresinas.oruc.com.br/arquivos/bi/1582235045_emailicompng.png');
 
-
+$('.newsletter .block_content ').addClass("block1");
 if (window.matchMedia('(max-width: 991px)').matches){
  
 $('.col-newsletter').removeClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
 $('.newsletter .text-center').removeClass("col-xs-4 col-sm-4 col-md-3 pull-left");
-$('.newsletter .block_content ').removeClass("col-xs-8 col-sm-8 col-md-9 pull-left");
+$('.newsletter .block1 ').removeClass("col-xs-8 col-sm-8 col-md-9 pull-left");
 $('.col-form-newsletter').removeClass("col-xs-12");
 $('#form_whatsapp .block_content:nth-child(1)').removeClass("col-xs-12 col-sm-12 col-md-5 ");
 $('#form_whatsapp .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
@@ -30,7 +30,7 @@ else{
    
 $('.col-newsletter').addClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
 $('.newsletter .text-center').addClass("col-xs-4 col-sm-4 col-md-3 pull-left");
-$('.newsletter .block_content ').addClass("col-xs-8 col-sm-8 col-md-9 pull-left");
+$('.newsletter .block1 ').addClass("col-xs-8 col-sm-8 col-md-9 pull-left");
 $('.col-form-newsletter').addClass("col-xs-12");
 $('#form_whatsapp .block_content:nth-child(1)').addClass("col-xs-12 col-sm-12 col-md-5");
 $('#form_whatsapp .block_content:nth-child(2)').addClass("col-xs-10 col-sm-10 col-md-5");
@@ -51,7 +51,7 @@ $(window).resize(function(){
  
 $('.col-newsletter').removeClass("col-lg-6 col-md-6 col-sm-6 col-xs-12");
 $('.newsletter .text-center').removeClass("col-xs-4 col-sm-4 col-md-3 pull-left");
-$('.newsletter .block_content ').removeClass("col-xs-8 col-sm-8 col-md-9 pull-left");
+$('.newsletter .block1 ').removeClass("col-xs-8 col-sm-8 col-md-9 pull-left");
 $('.col-form-newsletter').removeClass("col-xs-12");
 $('#form_whatsapp .block_content:nth-child(1)').removeClass("col-xs-12 col-sm-12 col-md-5 ");
 $('#form_whatsapp .block_content:nth-child(2)').removeClass("col-xs-10 col-sm-10 col-md-5");
