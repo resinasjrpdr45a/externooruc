c68e7b6aa10b24e8db98ddc42a2c27f65a94cd0a
Luis Fernando <serraluisfernando@gmail.com>
2020-05-07 16:29:05 -0300

    ajuste global

--- removedor-checkout.js
+++ removedor-checkout.js
@@ -1,18 +1,12 @@
 $(document).ready(function(){
- 	if (window.matchMedia('(max-width: 767px)').matches){       		
-			$('.checkout .table-summary a span').removeClass("hidden-xs");
-			
-		}  
+ 	$('.checkout .table-summary a span').removeClass("hidden-xs");
 
 		
 
 
 	$(window).resize(function(){
-		if (window.matchMedia('(max-width: 767px)').matches){        		
-			$('.checkout .table-summary a span').removeClass("hidden-xs");
-			
-		}  
-
+		$('.checkout .table-summary a span').removeClass("hidden-xs");
+		
 		
 		
 	});		       
