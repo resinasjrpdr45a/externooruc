96b454f11f9d03d26953ffb56feddb3ee190f6bc
Luis Fernando <serraluisfernando@gmail.com>
2020-06-04 14:28:39 -0300

    ajuste content page

--- menu-pagi-client.js
+++ menu-pagi-client.js
@@ -1,17 +1,21 @@
 $( document ).ready(function() {
-	$('#left_column .block .block_content').css({"display":"none"}); 
-	$('#left_column .block h4').click(function(){
- 		$('#left_column .block h4').toggleClass("clicado");
+	if (window.matchMedia('(max-width: 991px)').matches){
 		$('#left_column .block .block_content').css({"display":"none"}); 
-		$('#left_column .block .block_content').toggleClass("menucat");
-	});
-
-	$(window).resize(function(){
-		$('#left_column .block .block_content').css({"display":"none"}); 
- 		$('#left_column .block h4').click(function(){
+		$('#left_column .block h4').click(function(){
  			$('#left_column .block h4').toggleClass("clicado");
 			$('#left_column .block .block_content').css({"display":"none"}); 
 			$('#left_column .block .block_content').toggleClass("menucat");
 		});
+	}
+
+	$(window).resize(function(){
+		if (window.matchMedia('(max-width: 991px)').matches){
+			$('#left_column .block .block_content').css({"display":"none"}); 
+ 			$('#left_column .block h4').click(function(){
+ 				$('#left_column .block h4').toggleClass("clicado");
+				$('#left_column .block .block_content').css({"display":"none"}); 
+				$('#left_column .block .block_content').toggleClass("menucat");
+			});
+ 		}
 	});	
 });
