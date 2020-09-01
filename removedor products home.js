c2630d6cb8b8fd8e5c003f0c2e5a1ee31921c9a0
Luis Fernando <serraluisfernando@gmail.com>
2020-09-01 13:58:42 -0300

    ajuste de funcao

--- removedor products home.js
+++ removedor products home.js
@@ -5,6 +5,14 @@ if (window.matchMedia('(max-width: 475px)').matches){
    $('#index .ajax_block_product').addClass("col-xs-6  col-sm-4  col-md-3  col-lg-3 ");
 }
 
+ $(window).resize(function(){
+    if (window.matchMedia('(max-width: 475px)').matches){
+   		$('#index .ajax_block_product').removeClass("col-xs-6  col-sm-4  col-md-3  col-lg-3");
+	} else{
+   		$('#index .ajax_block_product').addClass("col-xs-6  col-sm-4  col-md-3  col-lg-3 ");
+	}
+  });
+
 
 
 });
