d2b3e691fc116f3a9f09b773b5b4cd9be99cac3f
Luis Fernando <serraluisfernando@gmail.com>
2020-09-01 13:53:09 -0300

    ajuste do removedor de colunas home

---
+++ removedor products home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+if (window.matchMedia('(max-width: 475px)').matches){
+   $('#index .ajax_block_product').removeClass("col-xs-6  col-sm-4  col-md-3  col-lg-3");
+} else{
+   $('#index .ajax_block_product').addClass("col-xs-6  col-sm-4  col-md-3  col-lg-3 ");
+}
+
+
+
+});
+
+
